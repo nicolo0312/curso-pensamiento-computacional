4d3af6adbe3cacbf924319b34a3743a256894fd3
moranni <moranni@globalhitss.com>
2021-04-09 12:15:52 -0300

    first commit

---
+++ programas_ramificados.py
@@ -0,0 +1,10 @@
+numero_1 = int(input('Escoge un entero: '))
+numero_2 = int(input('Escoge otro  entero: '))
+
+if numero_1 < numero_2:
+    print('El primer numero es menor que el segundo')
+elif numero_1 > numero_2:
+    print('El primer numero es mayor que el segundo')
+elif numero_1 == numero_2:
+    print('El primer numero es igual al segundo')
+    
